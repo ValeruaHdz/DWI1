d2f94f3eefc6ba2920cc974c89662afca182410b
Valeria Hernandez <2237000570@utna.edu.mx>
2025-06-26 19:36:26 -0600

    Se realizo la conexion de mongoose a mongodb

---
+++ app/config/conexion.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const CONFIG = require('./configuracion')
+const e = require('express')
+
+module.exports={
+    conection : null,
+    connect : ()=>{
+        if(this.conection)return this.conection
+        return mongoose.connect(CONFIG.DB)
+        .then(conn =>{
+            this.conection = conn
+            console.log('la conexion se realizo con exito')
+        })
+        .catch(e => console.log('error en la conexion', e))
+    }
+
+}
